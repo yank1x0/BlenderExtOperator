8c5262425079cd300800301ef780dff4c1e4ee14
RAMI-PC\rami <yank.rami@gmail.com>
2018-05-19 16:02:17 +0300

    explanation update

--- bin/main_modeller.py
+++ bin/main_modeller.py
@@ -7,7 +7,9 @@ summary:
 -----------
 this is an example of blender operator that is coupled with blender instance but has independent GUI (Tkinter-based in this example),
 unlike the usual blender operator extensions that are meant to be accessed from blender GUI itself.
-in its basis, the idea is to separate the blender thread cycle and 
+in its basis, the idea is to add a break in the main blender thread execution and execute delegated methods inside that break, this way making the blender environment
+accessible to them.
+
 note this is just sample of a single projoect component, not the full project. multiple pre-requisites and configurations are required for the full project.
 the purpose is demonstration only.
 
